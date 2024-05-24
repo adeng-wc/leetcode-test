d12272faae77b0d5a6104d61afcafab3aa4e9392
翁某小小 <wengcheng.no1@gmail.com>
2024-05-24 18:15:53 +0800

    leetcode，回溯算法 解决组合问题

---
+++ src/main/java/com/adeng/leetcode/array/hard/test.java
@@ -0,0 +1,18 @@
+package com.adeng.leetcode.array.hard;
+
+/**
+ *
+ 给定一个集合，有N个正整数（int32），
+判断（求解）是否存在一个子集，子集和等于X, 输出一个子集
+ 数据范围1 ：1 <= N <= 20,   1 <= X <= 10^9
+ 数据范围2: 1 <= N <= 5000, 1 <= X <= 10^4
+ input1:
+ 5
+ 9 13 23 30
+ 36
+ output1:
+ 13 23
+ */
+public class test {
+
+}
---
+++ src/main/java/com/adeng/leetcode/backtracking/middle/test_77.java
@@ -0,0 +1,41 @@
+package com.adeng.leetcode.backtracking.middle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 77. 组合（不强调元素顺序）
+ * <p>
+ * https://leetcode.cn/problems/combinations/description/
+ */
+public class test_77 {
+
+    List<List<Integer>> result = new ArrayList<>();
+    List<Integer> subList = new ArrayList<>();
+
+    public List<List<Integer>> combine(int n, int k) {
+        backTracking(1, n, k);
+        return result;
+    }
+
+    private void backTracking(int startIndex, int n, int k) {
+        // 退出条件
+        if (subList.size() == k) {
+            result.add(new ArrayList<>(subList));
+            return;
+        }
+
+        // 可以剪枝的地方就在递归中每一层for循环所选择的起始位置
+        // 当剩余个数，不足 (k-subList.size()) 的时候，就可以停止遍历
+        for (int i = startIndex; i <= n-(k-subList.size())+1; i++) {
+            subList.add(i);
+            backTracking(i + 1, n, k);
+            subList.removeLast();
+        }
+    }
+
+    public static void main(String[] args) {
+        test_77 t = new test_77();
+        List<List<Integer>> combine = t.combine(4, 2);
+    }
+}
